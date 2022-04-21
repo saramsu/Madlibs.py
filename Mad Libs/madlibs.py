b4c6d5e791ee73b6d0f062473ccfd01739b845d6
Sara Amaya <98931088+saramsu@users.noreply.github.com>
2022-04-21 00:10:55 -0500

    Add files via upload
    Basic MadLibs game with a story in Spanish. :)

---
+++ Mad Libs/madlibs.py
@@ -0,0 +1,11 @@
+adj = input("Adjetivo: ")
+verb1 = input("Verbo: ")
+verb2 = input("Verbo: ")
+famous_person = input("Nombre de famoso: ")
+
+
+
+
+madlib = f"Mi primer día de vacaciones me encontré con alguien que parecía muy {adj} Me parece que es porque le gusta mucho \ {verb1}, luego de una hora de verle me di cuenta de que era {famous_person} y entonces yo empecé {verb2}!."
+
+print(madlib)
